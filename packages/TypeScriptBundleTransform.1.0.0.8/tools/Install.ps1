355c8ef0d55a4ed0c11fb4b64c8ac8df9a91f31b
Arkady Turchenko <arkady.turchenko@aerdata.com>
2015-06-10 17:30:09 +0200

    Excluede packages

--- packages/TypeScriptBundleTransform.1.0.0.8/tools/Install.ps1
+++
@@ -1,9 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-
-$item = $project.ProjectItems.Item("v8").ProjectItems.Item("Noesis.Javascript.x64.dll")
-$item.Properties.Item("BuildAction").Value = 0
-$item.Properties.Item("CopyToOutputDirectory").Value = 1
-
-$item = $project.ProjectItems.Item("v8").ProjectItems.Item("Noesis.Javascript.x86.dll")
-$item.Properties.Item("BuildAction").Value = 0
-$item.Properties.Item("CopyToOutputDirectory").Value = 1
